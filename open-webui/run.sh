1262cb3ba351e4ec76e0f2c93f0f626bd93045a7
Wojciech Lichota <wojciech@lichota.pl>
2025-01-21 23:51:02 +0100

    Update run.sh

--- open-webui/run.sh
+++ open-webui/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export OPENAI_API_KEY=`python -c "import json; print(json.loads(open('/data/options.json').read()).get('OPENAI_API_KEY', ''))"`
-
-echo OPENAI_API_KEY
-echo $OPENAI_API_KEY
+export ENABLE_OLLAMA_API=false
 
 open-webui serve
