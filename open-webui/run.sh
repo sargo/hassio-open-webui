9212715469f63a874ff50bb393125a418a5aaa3d
Wojciech Lichota <wojciech@lichota.pl>
2025-01-21 21:42:25 +0100

    Update run.sh

--- open-webui/run.sh
+++ open-webui/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OPENAI_API_KEY="TBD"
+export OPENAI_API_KEY=`python -c "import json; print(json.loads(open('/data/options.json').read()).get('OPENAI_API_KEY', ''))"`
 
 echo OPENAI_API_KEY
 echo $OPENAI_API_KEY
